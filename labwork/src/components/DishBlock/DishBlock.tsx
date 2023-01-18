8bf4b0614a43374b22c3c980737284073d46ac10
danil tsoy <abdullaevdanil2002@gmail.com>
2023-01-18 12:39:37 +0600

    #1 added page to add new dishes + added new container for add dish form and checked for working correctly/ Planning to work with editing dish and deleting it

--- labwork/src/components/DishBlock/DishBlock.tsx
+++ labwork/src/components/DishBlock/DishBlock.tsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import './DishBlock.css'
 import IDishBlockProps from './IDishBlockProps'
+import './DishBlock.css'
 
 const DishBlock: React.FunctionComponent<IDishBlockProps> = (props): React.ReactElement => {
 
     return(
         <div className='DishBlock'>
-            <div>
-                <img src={props.dish.imageSrc} alt={`${props.dish.name}_image`}/>
+            <div className='DishBlock__inner'>
+                <img className='DishBlock_image' src={props.dish.imageSrc} alt={`${props.dish.name}_image`}/>
                 <p>{props.dish.name}: {props.dish.cost} KZT</p>
             </div>
-            <div>
+            <div className='DishBlock__inner'>
                 <button onClick={props.editDish}>Edit</button>
                 <button onClick={props.deleteDish}>Delete</button>
             </div>
--- labwork/src/containers/AddDishForm/AddDishForm.tsx
+++ labwork/src/containers/AddDishForm/AddDishForm.tsx
@@ -1,11 +1,82 @@
-import React from "react";
+import React, { ChangeEvent, FormEvent, useEffect, useState } from 'react'
+import { useSelector } from "react-redux";
+import IDish from "../../interfaces/IDish";
+import { postNewDish } from '../../store/Menu/menu.slice';
+import { AppState, useAppDispatch } from "../../store/store";
 import './AddDishForm.css'
 
 const AddDishForm: React.FunctionComponent = (): React.ReactElement => {
 
-    return(
-        <div>
+    const dispatch = useAppDispatch()
 
+    const {loading} = useSelector((state: AppState) => state.menu)
+
+    const [dish, setDish] = useState<IDish>(
+        {
+            name: '',
+            cost: 0,
+            imageSrc: ''
+        }
+    )
+
+    
+    const checkButton = (): void => {
+        if (dish.name.trim() === '' || dish.imageSrc.trim() === '' || dish.cost <= 0){
+            setButtonDisabled(true)
+            return
+        }
+        setButtonDisabled(false)
+    }
+
+    const [buttonDisabled, setButtonDisabled] = useState<boolean>(true)
+
+    const inputHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        const {name, value} = event.target
+        setDish(prevState => {
+            return {...prevState, [name]: value}
+        })
+    }
+
+    const submitDish = (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+        dispatch(postNewDish(dish))
+        setDish({
+            name: '',
+            cost: 0,
+            imageSrc: ''
+        })
+    }
+
+    useEffect(() => {
+        checkButton()
+    }, [dish])
+
+    return(
+        <div className="AddDishForm">
+            {
+                loading ? 
+                <h1>Loading...</h1> :
+                <>
+                    <form onSubmit={(event) =>{submitDish(event)}}>
+                        <div className='AddDishForm_inner_block'>
+                            <p>Dish</p>
+                            <input type={'text'} name={'name'} onChange={(event)=>{inputHandler(event)}}/>
+                        </div>
+                        <div className='AddDishForm_inner_block'>
+                            <p>Cost</p>
+                            <input type={'number'} name={'cost'} onChange={(event)=>{inputHandler(event)}}/>
+                        </div>
+                        <div className='AddDishForm_inner_block'>
+                            <p>Image</p>
+                            <input type={'text'} name={'imageSrc'} onChange={(event)=>{inputHandler(event)}}/>
+                        </div>
+                        <button
+                        disabled={buttonDisabled}
+                        className="AddForm__button"
+                    >Save</button>                       
+                    </form>
+                </>
+            }
         </div>
     )
 }
